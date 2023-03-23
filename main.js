8fd3c0650295e6c94b9e70016148bae0b4bb5367
권세명 <3pkwon@gmail.com>
2023-03-23 16:40:27 +0900

    TIL

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
+const QUOTES = "quotes";
+
 function getTime() {
   const time = document.querySelector(".time");
-
   const newDate = new Date();
 
   // const hours = String(newDate.getHours()).padStart(2, "0");
@@ -21,3 +22,58 @@ function getTime() {
 
 getTime();
 setInterval(getTime, 1000);
+
+function getQuotes() {
+  const quotesMsg = document.querySelector(".quotesMsg");
+  let savedQuotes = localStorage.getItem(QUOTES);
+
+  if (!savedQuotes) {
+    localStorage.setItem(
+      QUOTES,
+      JSON.stringify([
+        "화이팅!!!!!!",
+        "Let's go!!!!!!",
+        "코딩 안 어렵다;;",
+        "코딩 좋다;;;",
+        "Run",
+      ])
+    );
+
+    savedQuotes = localStorage.getItem(QUOTES);
+  }
+
+  let quotesArray = JSON.parse(savedQuotes);
+
+  quotesMsg.innerText =
+    quotesArray[Math.floor(Math.random() * quotesArray.length)];
+}
+
+getQuotes();
+
+function onClickAdd() {
+  const newQuotes = document.querySelector(".newQuotes");
+
+  newQuotes.style.display = "inline-block";
+}
+
+function onClickRegist() {
+  const quotesMsg = document.querySelector(".quotesMsg");
+  const newQuotes = document.querySelector(".newQuotes");
+  const newQuotesInput = document.querySelector(".newQuotesInput");
+
+  if (!newQuotesInput.value) {
+    alert("입력 please :)");
+    return;
+  }
+
+  let savedQuotes = localStorage.getItem(QUOTES);
+
+  let quotesArray = JSON.parse(savedQuotes);
+  quotesArray.push(newQuotesInput.value);
+
+  localStorage.setItem(QUOTES, JSON.stringify(quotesArray));
+
+  quotesMsg.innerText = newQuotesInput.value;
+  newQuotes.style.display = "none";
+  newQuotesInput.value = "";
+}
