1657fb805eb81bf469e6ede5c58fe9a884b3b69d
권세명 <3pkwon@gmail.com>
2023-03-27 16:25:36 +0900

    Monday Finished

--- main.js
+++ main.js
@@ -119,3 +119,16 @@ async function onClickSearch() {
   searchInput.value = "";
   isLoading = false;
 }
+
+function onClickToggle(value) {
+  const nft = document.querySelector(".nft");
+  const nftView = document.querySelector(".nftView");
+
+  if (value) {
+    nft.style.display = "inline-block";
+    nftView.style.display = "none";
+  } else {
+    nft.style.display = "none";
+    nftView.style.display = "inline-block";
+  }
+}
