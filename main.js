dd4dfb5579c63b8cfaf88d2cf208dc230e0b7d39
권세명 <3pkwon@gmail.com>
2023-03-27 15:23:51 +0900

    ChagGPT

--- main.js
+++ main.js
@@ -12,12 +12,14 @@ function getTime() {
   // time.innerText = `${hours}:${minutes}:${seconds}`;
 
   const timeString = newDate.toLocaleTimeString([], {
-    hour: "numeric",
+    hour: "2-digit",
     minute: "numeric",
-    second: "numeric",
+    // second: "numeric",
     hour12: true,
   });
   time.innerText = timeString;
+
+  const fullDate = newDate.getDate();
 }
 
 getTime();
@@ -76,4 +78,44 @@ function onClickRegist() {
   quotesMsg.innerText = newQuotesInput.value;
   newQuotes.style.display = "none";
   newQuotesInput.value = "";
+}
+
+// 3월27일
+
+let isLoading = false;
+
+async function onClickSearch() {
+  const searchInput = document.querySelector(".searchInput");
+  const searchResult = document.querySelector(".searchResult");
+
+  //값이 없거나, 로딩중이면 return
+  if (!searchInput.value) return;
+  if (isLoading) return;
+
+  // 여기까지 왔다는 의미 === isLoading false
+  isLoading = true;
+  const question = searchInput.value;
+  searchInput.value = "검색 중 입니다... 잠시만 기다려주세요.";
+
+  // 프론트엔드에서 백엔드
+  const response = await axios.post(
+    "https://holy-fire-2749.fly.dev/chat",
+    {
+      question,
+    },
+    {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: "Bearer BLOCKCHAINSCHOOL3",
+      },
+    }
+  );
+
+  if (response.status === 200) {
+    searchResult.style.display = "inline";
+    searchResult.innerText = response.data.choices[0].message.content;
+  }
+
+  searchInput.value = "";
+  isLoading = false;
 }
